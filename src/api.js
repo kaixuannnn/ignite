810d4f6f8eb5cfe851466b49478f6aac96cf452a
kaixuannnn <kaixuant@yahoo.com>
2020-12-08 22:18:44 +0800

    fetch first data

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // Base URL
-const base_url = "https://api.rawg.io/api";
+const base_url = "https://api.rawg.io/api/";
 
 //Getting the date
 const getCurrentMonth = () => {
@@ -22,7 +22,11 @@ const getCurrentDay = () => {
 
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
-const currentDay = getCurrrentDay();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 //Popular Games
-const popular_games = "https://api.rawg.io/api/games?";
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
+export const popularGamesURL = () => `${base_url}${popular_games}` 
